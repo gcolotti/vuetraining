299dba31016990c9704f1014f6b725b64c215b8d
Guillermo Colotti <guille.theoffspring@gmail.com>
2021-06-28 15:33:17 -0300

    Render external html

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ const app = new Vue({
     message: 'Hello World!',
     sum: 3 + 6,
     value: 2,
-    people: ["Guille", "Juan", "Nati", "Pedro"]
+    people: ["Guille", "Juan", "Nati", "Pedro"],
+    title: "<h1>Zerg Swarm</h1>",
+    imageSrc: 'https://bnetcmsus-a.akamaihd.net/cms/blog_header/u0/U0EAW79NTVXP1498164245687.jpg',
+    imageTitle: 'Zerg Swarm',
   },
   computed: {
     reverseString: function () {
